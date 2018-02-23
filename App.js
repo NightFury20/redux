e18603e4fe63767efeef21e03bcfceeb6641758c
NightFury20 <michael@itmates.co.za>
2018-02-23 15:33:44 +0200

    Creating Selection Reducer

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={styles.container}>
+          <Header title='Redux' />
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
